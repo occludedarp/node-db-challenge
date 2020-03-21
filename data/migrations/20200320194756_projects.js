9e638c2bcef76e51aaef2da454cc7a2f61106b80
Cameron Moore <cmrnmr@gmail.com>
2020-03-21 09:31:05 -0400

    editing schemas

--- data/migrations/20200320194756_projects.js
+++ data/migrations/20200320194756_projects.js
@@ -1,16 +1,15 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('projects', table => {
-    table.increments();
-
+  return knex.schema
+  .createTable('projects', table => {
+    table.increments('project_id');
     table.string('name', 30);
-
-    table.text('description');
-
+    table.text('project_description');
     table.boolean('completed').notNullable().defaultTo(false);
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('projects');
+  return knex.schema
+  .dropTableIfExists('projects')
 };
---
+++ data/migrations/20200321091401_tasks.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('tasks', table => {
+    table.increments('id');
+    table.foreign('Project_id').references('projects.project_id');
+    table.string('task_description', 120);
+    table.string('notes', 120);
+    table.boolean('completed').notNullable().defaultTo(false);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('tasks');
+};
